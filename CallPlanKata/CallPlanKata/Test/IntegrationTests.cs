58d71d5d0784274c7562bd87be4e49c72ab53c7e
Matthew Swarbrick <matt_swarbrick@hotmail.com>
2014-09-11 16:32:45 +0100

    Added failing test around odd web service response

--- CallPlanKata/CallPlanKata/Test/IntegrationTests.cs
+++ CallPlanKata/CallPlanKata/Test/IntegrationTests.cs
@@ -41,6 +41,34 @@ namespace CallPlanKata
             StringAssert.Contains("Invoke function with \"bob@test.com\", response is \""+webServiceResponse+"\"", result);
             StringAssert.Contains("Deliver to group \"A\" and Agent \"1\"", result);
         }
+
+        [TestCase(3)]
+        [TestCase(1)]
+        public void GivenAnInteractionComesIntoTheSystemWhenTheWebServiceRespondsWithAnOddNumberThenTheInteractionGetsAssignedToAnAgentInGroupB(int webServiceResponse)
+        {
+            var interaction = new Interaction()
+            {
+                Id = "bob@test.com",
+                Type = InteractionType.email
+            };
+
+            var getOrginatorSpecificDataStep = new GetOriginatorSpecificDataStep(_fakeWebService);
+            var agentAssigningStep = new AgentAssigningStep();
+
+            var callPlan = new CallPlan();
+            callPlan.AppendStep(getOrginatorSpecificDataStep);
+            callPlan.AppendStep(agentAssigningStep);
+
+            _fakeWebService.Stub(fws => fws.GetOriginatorSpecificData(Arg<Interaction>.Is.Equal(interaction))).Return(webServiceResponse);
+
+            callPlan.ReceiveInteraction(interaction);
+
+            var result = callPlan.PrintSummary();
+
+            StringAssert.Contains("Receive email from \"bob@test.com\"", result);
+            StringAssert.Contains("Invoke function with \"bob@test.com\", response is \""+webServiceResponse+"\"", result);
+            StringAssert.Contains("Deliver to group \"B\" and Agent \"1\"", result);
+        }
     }
 }
 
